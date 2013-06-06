a7588f2504260a15bdb24ae5ff42491595c365ff
Massimo <massimozanini86@gmail.com>
2013-06-06 21:20:01 +0200

    Sdk min version to 12, added hardware acceleration, cleaned

--- src/it/chiantibanca/mobile/ChbWidget.java
+++ src/it/chiantibanca/mobile/ChbWidget.java
@@ -5,14 +5,13 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.view.View;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 import android.util.Log;
 
 public class ChbWidget extends AppWidgetProvider {
 	
-	private static final String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
+	//private static final String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
 	//private static final String ACTION_WIDGET_CONFIGURE = "ActionConfigure";
 	
 	@Override
